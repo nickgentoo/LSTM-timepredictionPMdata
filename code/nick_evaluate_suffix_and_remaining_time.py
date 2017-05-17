98bc90331fe454ba8487b59f89002c1e037c7c99
Nicolò Navarin <nnavarin@math.unipd.it>
2017-05-16 16:01:05 -0800

    evaluation bugfix

--- code/nick_evaluate_suffix_and_remaining_time.py
+++ code/nick_evaluate_suffix_and_remaining_time.py
@@ -277,6 +277,7 @@ two_ahead_pred = []
 three_ahead_gt = []
 three_ahead_pred = []
 
+cropped_attributes=[[] for i in xrange(len(attributes))]
 
 # make predictions
 with open('output_files/results/'+fileprefix+'_suffix_and_remaining_time_%s' % eventlog, 'wb') as csvfile:
@@ -290,8 +291,12 @@ with open('output_files/results/'+fileprefix+'_suffix_and_remaining_time_%s' % e
             cropped_line = ''.join(line[:prefix_size])
             cropped_times = times[:prefix_size]
             cropped_times3 = times3[:prefix_size]
-            cropped_attributes= [a[:prefix_size] for a in attributes]
-            print cropped_attributes
+            for a in xrange(len(attributes)):
+                # print(attributes[a][0:i])
+                cropped_attributes[a].append(attributes[a][0:i])
+
+            #cropped_attributes= [a[:prefix_size] for a in attributes]
+            #print cropped_attributes
 
             if len(times2)<prefix_size:
                 continue # make no prediction for this case, since this case has ended already
